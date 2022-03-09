43723e0f215221b9a3c62bcc3b735d1ddaf25c22
Raymond Chen <ahrchen@gmail.com>
2022-03-09 14:24:52 -0800

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Raymond Chen on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
